224b0fc0917a9952aa01051b7fa59ae68334e8a9
lixueping <lixueping@lixuepingdeiMac.local>
2018-02-23 03:25:50 +0800

    完成添加 多对多什么的

--- models/register.go
+++ models/register.go
@@ -35,60 +35,53 @@ func RigsterStruct(data interface{}) map[string]string {
 	vv := reflect.ValueOf(data)
 	v := reflect.Indirect(vv)
 
-	// beego.Critical("API", v.Type().Name(), v.Type().PkgPath())
+	beego.Critical("API", v.Type().Name(), v.Type().PkgPath())
+	services := strings.Split(v.Type().PkgPath(), "/")
+	tmp["Services"] = services[len(services)-2]
 	// vv.MethodByName().FieldByName()
 	// vv.MethodByName().IsValid
 
 	tmp["Struct"] = v.Type().Name()
 	for i := 0; i < v.NumField(); i++ {
-		//利用反射获取structTag
-		tmp["Tag"] = fmt.Sprintf("%s", v.Type().Field(i).Tag)
-		// html 字段类型
-		if v.Type().Field(i).Tag.Get("colType") != "" {
-			tmp["ColType"] = v.Type().Field(i).Tag.Get("colType")
-		} else {
-			tmp["ColType"] = v.Type().Field(i).Type.String()
-		}
-		beego.Critical("ColType", tmp["ColType"])
-		switch tmp["ColType"] {
-		case "radio":
-			tmp["detail"] = v.Type().Field(i).Tag.Get("radio")
-		case "manytomany":
-			tmp["detail"] = v.Type().Field(i).Tag.Get("manytomany")
-		case "select":
-			tmp["detail"] = v.Type().Field(i).Tag.Get("select")
-		case "multiselect":
-			tmp["detail"] = v.Type().Field(i).Tag.Get("multiselect")
-		}
+		if strings.ToLower(v.Type().Field(i).Name) != "id" && strings.ToLower(v.Type().Field(i).Name) != "create" && strings.ToLower(v.Type().Field(i).Name) != "update" {
+			//利用反射获取structTag
+			tmp["Tag"] = fmt.Sprintf("%s", v.Type().Field(i).Tag)
+			// html 字段类型
+			if v.Type().Field(i).Tag.Get("colType") != "" {
+				tmp["ColType"] = v.Type().Field(i).Tag.Get("colType")
+			} else {
+				tmp["ColType"] = v.Type().Field(i).Type.String()
+			}
+			beego.Critical("ColType", tmp["ColType"])
+			switch tmp["ColType"] {
+			case "radio":
+				tmp["detail"] = v.Type().Field(i).Tag.Get("radio")
+			case "manytomany":
+				tmp["detail"] = v.Type().Field(i).Tag.Get("manytomany")
+			case "select":
+				tmp["detail"] = v.Type().Field(i).Tag.Get("select")
+			case "multiselect":
+				tmp["detail"] = v.Type().Field(i).Tag.Get("multiselect")
+			case "o2o":
+				tmp["detail"] = v.Type().Field(i).Tag.Get("o2o")
+			case "o2m":
+				tmp["detail"] = v.Type().Field(i).Tag.Get("o2m")
+			}
 
-		//字段名 供前端form表单使用
-		if v.Type().Field(i).Tag.Get("name") != "" {
-			tmp["Name"] = v.Type().Field(i).Tag.Get("name")
-		} else {
-			tmp["Name"] = v.Type().Field(i).Name
+			//字段名 供前端form表单使用 还有table字段
+			if v.Type().Field(i).Tag.Get("name") != "" {
+				tmp["Name"] = v.Type().Field(i).Tag.Get("name")
+			} else {
+				tmp["Name"] = v.Type().Field(i).Name
+			}
+			tmp["Names"] += fmt.Sprintf("%s ", tmp["Name"])
+			//收集昵称
+			if v.Type().Field(i).Tag.Get("verbose_name") != "" {
+				tmp["Col"] = fmt.Sprintf("%s %s:%s:%s:%s", tmp["Col"], v.Type().Field(i).Tag.Get("verbose_name"), tmp["ColType"], tmp["Name"], tmp["detail"])
+			} else {
+				tmp["Col"] = fmt.Sprintf("%s %s:%s:%s:%s", tmp["Col"], v.Type().Field(i).Name, tmp["ColType"], tmp["Name"], tmp["detail"])
+			}
 		}
-		//收集昵称
-		if v.Type().Field(i).Tag.Get("verbose_name") != "" {
-			tmp["Col"] = fmt.Sprintf("%s %s:%s:%s:%s", tmp["Col"], v.Type().Field(i).Tag.Get("verbose_name"), tmp["ColType"], tmp["Name"], tmp["detail"])
-		} else {
-			tmp["Col"] = fmt.Sprintf("%s %s:%s:%s:%s", tmp["Col"], v.Type().Field(i).Name, tmp["ColType"], tmp["Name"], tmp["detail"])
-		}
-
-		//
-
-		// fmt.Println("Tag", v.Type().Field(i).Tag)
-		// fmt.Println("Tag", v.Type().Field(i).Tag)
-		// fmt.Println("Type", v.Type().Field(i).Type)
-		// fmt.Println("Name", v.Type().Field(i).Name)
-		// fmt.Println("Index", v.Type().Field(i).Index)
-		// fmt.Println("Offset", v.Type().Field(i).Offset)
-		// fmt.Println("pkgpath", v.Type().Field(i).PkgPath)
-		// fmt.Println(st.Field(i).Tag.Get("search"))
-		// for _, y := range strings.Split(tmp["Tag"], " ") {
-		// 	rs := strings.Split(y, ":")
-		// 	tmp[v.Type().Field(i).Name+"_"+rs[0]] = rs[1]
-		// }
-		fmt.Println("DDDDDDDDDDD", v.Type().Field(i).Tag.Get("verbose_name"))
 	}
 	return tmp
 }
